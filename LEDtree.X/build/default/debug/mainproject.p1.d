3db5d545cefa47863d4fe519b448effbeb9a7a9e
Joe Hung <jjoe0303@gmail.com>
2017-12-22 00:30:24 +0800

    Add shift in same layer + gradient color change + send interrupt to another pic18

--- LEDtree.X/build/default/debug/mainproject.p1.d
+++
@@ -1,3 +0,0 @@
- build/default/debug/mainproject.d  \
- build/default/debug/mainproject.p1:  \
- mainproject.c 
---
+++ LEDtree.X/build/default/production/mainproject.p1.d
@@ -0,0 +1,3 @@
+ build/default/production/mainproject.d  \
+ build/default/production/mainproject.p1:  \
+ mainproject.c 
